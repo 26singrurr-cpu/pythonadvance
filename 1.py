1cf351fb49c3c7611db520a0a40cace37ca7cc71
26singrurr-cpu <26singrurr@rbunagpur.in>
2026-09-24 17:05:10 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,9 @@
+name = "Riya"
+age = 18
+college = "RBU"
+branch = "ECS"
+
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+print("Branch:", branch)
---
+++ 20.py
@@ -0,0 +1,15 @@
+marks = float(input("Enter marks: "))
+if marks >= 90:
+ print("Grade A")
+elif marks >= 80:
+ print("Grade B")
+elif marks >= 70:
+ print("Grade C")
+elif marks >= 60:
+ print("Grade D")
+elif marks >= 40:
+ print("Grade E")
+elif marks >= 0:
+ print("Grade F")
+else:
+ print("Invalid marks")
